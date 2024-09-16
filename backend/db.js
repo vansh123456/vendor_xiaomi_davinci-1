b0c5a41a5255af71379bc1cf1c5d477d4f7469b0
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-09-16 17:06:04 +0530

    init db

---
+++ backend/db.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose")
+mongoose.connect('mongodb+srv://vanshbhatnagar14dev:129212@cluster0.blv3ioj.mongodb.net/');
+console.log("db connect done");
+const {Schema} = mongoose;
+const fetchSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    last: {
+        type: Number,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    buy: {
+        type: Number,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    sell: {
+        type: Number,
+        required: true,
+        unique: false,
+        trim: true,
+    },
+    volume: {
+        type: Number,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    base_unit: {
+        type: Number,
+        required: true,
+        unique: false,
+        trim: true
+    }
+})
+console.log("schema defined");
+const hodlinfo = mongoose.model('hodlinfo',fetchSchema);
+console.log('model initiated');
+module.exports= {
+    hodlinfo
+}
